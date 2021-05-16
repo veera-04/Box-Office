193802e18087ef426019701d62bbf3a2f401b3b7
Veera <veera4721@gmail.com>
2021-05-16 13:08:48 +0530

    Added nav bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        This is home page
-      </Route>
-      <Route exact path="/starred">
-        This is starred
-      </Route>
-      <Route>
-        <h2>Error:404 </h2>Page not Found
-      </Route>
-    </Switch>
+    <div>
+      <Navs />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route>
+          <h2>Error:404 </h2>Page not Found
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
