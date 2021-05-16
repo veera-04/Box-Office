d53b468cfd1f2a908e27544311f92138ba374e24
Veera <veera4721@gmail.com>
2021-05-16 19:13:57 +0530

    displayed show results

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,37 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
   const onInputChange = ev => {
     setInput(ev.target.value);
   };
   const onSearch = () => {
-    fetch(`http://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json)
-      .then(result => {
-        console.log(result);
-      });
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+    });
   };
   const onKeyDown = ev => {
     if (ev.keyCode === 13) {
       onSearch();
     }
+  };
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No Results</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+    return null;
   };
   return (
     <MainPageLayout>
@@ -29,6 +44,7 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
