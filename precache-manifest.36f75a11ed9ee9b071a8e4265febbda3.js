357c88820d0378c24720d990da1f95afa4c01508
Veera <veera4721@gmail.com>
2021-06-20 13:38:19 +0530

    Updates

---
+++ precache-manifest.36f75a11ed9ee9b071a8e4265febbda3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e5e3a2bab3ff42986404faf9874db0",
+    "url": "/Box-Office/index.html"
+  },
+  {
+    "revision": "bcd51cd7ce0c593eb8db",
+    "url": "/Box-Office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "dd8b0f481ae577220037",
+    "url": "/Box-Office/static/js/2.03bdb265.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Box-Office/static/js/2.03bdb265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcd51cd7ce0c593eb8db",
+    "url": "/Box-Office/static/js/main.9b9a8f8f.chunk.js"
+  },
+  {
+    "revision": "6740a4f854f98d4fa166",
+    "url": "/Box-Office/static/js/runtime-main.140a3920.js"
+  }
+]);
